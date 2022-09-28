5b82e1c9a44b1e31486e0034d223e4e88d11ff53
Gagan K <gagan@innoventes.co>
2022-09-28 16:36:12 +0530

    changes

--- Room_Booking.js
+++ Room_Booking.js
@@ -1,153 +1,244 @@
-let room = document.getElementById("number1");
-room.value = 1; // Inititally assigning room number as 1
-let adult = document.getElementById("number2");
-adult.value = 1; // Initially assigning adult number as 1
+let room = document.getElementById("room");
+room.value = 1;
+let adult = document.getElementById("adult");
+adult.value = 1;
+let children = document.getElementById("children");
+children.value = 0;
 
-// Function to increement the count of the room number
-function increement_room() {
-    document.getElementById("btn2").style.color = 'blue';
-    document.getElementById("btn2").disabled = false;
-    document.getElementById("btn3").disabled = false;
-    document.getElementById("btn5").disabled = false;
-    document.getElementById("btn3").style.color = 'red';
-    document.getElementById("btn5").style.color = 'red';
-    document.getElementById("btn4").disabled = false;
-    document.getElementById("btn6").disabled = false;
-    document.getElementById("btn4").style.color = 'blue';
-    document.getElementById("btn6").style.color = 'blue';
-    room = document.getElementById("number1").value;  // getting the present value of the room number
-    adult = document.getElementById("number2").value; // getting the present value of the adult number
-    ++room;
-    ++adult; 
-    if(room>5) {                                   // if room count becomes greater than 5 
-        document.getElementById("btn1").disabled = true;    // increement button of the room section will be disabled
-        document.getElementById("btn1").style.color = 'black';  // correspondingly it is indicated by changing the button color to black
+let plus_room = document.getElementById("increment_room");
+let plus_adult = document.getElementById("increment_adult");
+let plus_children = document.getElementById("increment_children");
+
+let minus_room = document.getElementById("decrement_room");
+let minus_adult = document.getElementById("decrement_adult");
+let minus_children = document.getElementById("decrement_children");
+
+minus_room.disabled = true;
+minus_adult.disabled = true;
+minus_children.disabled = true;
+minus_room.style.color = 'black';
+minus_adult.style.color = 'black';
+minus_children.style.color = 'black';
+
+function increment_room() {
+    minus_room.disabled = false;
+    minus_room.style.color = 'blue';
+    minus_adult.disabled = false;
+    minus_adult.style.color = 'blue';
+    let room_count = parseInt(room.value);
+    room_count++;
+    if(room_count<6) {
+        if(room.value == adult.value) {
+            adult.value = parseInt(adult.value) + 1;
+        }
+        room.value = parseInt(room.value) + 1;
+        if(room_count == 5) {
+            plus_room.disabled = true;
+            plus_room.style.color = 'black';
+        }
     }
-    else if (room==adult) {                                 // if room count is equal to adult count
-        document.getElementById("number1").value = room;    // increement room count
-        document.getElementById("number2").value = adult;   // increement adult count
+}
+
+function increment_adult() {
+    minus_adult.disabled = false;
+    minus_adult.style.color = 'blue';
+    let adult_count = parseInt(adult.value);
+    let children_count = parseInt(children.value);
+    let return_type_1 = total_members(+1,adult_count+1,children_count);
+    if(return_type_1 == 1) {
+        adult.value = parseInt(adult.value) + 1;
     }
-    else {                                                  
-        document.getElementById("number1").value = room;    // normally increement the count
+    else if(return_type_1 == 0) {
+        room.value = parseInt(room.value) + 1;
+        adult.value = parseInt(adult.value) + 1;
+        if(room.value == 5) {
+            plus_room.disabled = true;
+            plus_room.style.color = 'black';
+        }
     }
+    else if(return_type_1 == 2) {
+        adult.value = parseInt(adult.value) + 1;
+        plus_adult.disabled = true;
+        plus_adult.style.color = 'black';
+        plus_children.disabled = true;
+        plus_children.style.color = 'black';
+    }
+}
+
+function increment_children() {
+    minus_children.disabled = false;
+    minus_children.style.color = 'blue';
+    children_count = parseInt(children.value);
+    adult_count = parseInt(adult.value);
     
-}
-
-// Function to decreement the count of the room number
-function decreement_room() {
-    document.getElementById("btn1").style.color = 'red';
-    document.getElementById("btn1").disabled = false;
-    document.getElementById("btn4").disabled = false;
-    document.getElementById("btn3").disabled = false;
-    document.getElementById("btn5").disabled = false;
-    document.getElementById("btn4").style.color = 'blue';
-    document.getElementById("btn3").style.color = 'red';
-    document.getElementById("btn5").style.color = 'red';
-    room = document.getElementById("number1").value;
-    adult = document.getElementById("number2").value;
-    children = document.getElementById("number3").value;
-    room--;                                                 // decreement the room count
-    people(room,adult,children);                            // calling people function
-
-}
-
-// Function to increement the count of the adult number
-function increement_adult() {
-    adult = document.getElementById("number2").value;
-    let re = summ();                                        // initialising the variable re and assigning the value returned from the function summ
-    if(re!=0) {                                             // if re is not equal to 0
-        document.getElementById("number2").value = ++adult;  // increement the count of the adult 
-        document.getElementById("btn4").disabled = false;
-        document.getElementById("btn4").style.color = 'blue';
+    let return_type_2 = total_members(+1,adult_count,children_count+1);
+    if(return_type_2==1) {
+        children.value = parseInt(children.value) + 1;
     }
-    else {                                                  // if re == 0
-        document.getElementById("btn3").disabled = true;    // disabling the increement button of the adult 
-        document.getElementById("btn3").style.color = 'black';  // indicating the same
+    else if(return_type_2==0) {
+        if(room.value == adult.value) { // if in a single room only 1 adult & 3 chiildren are present
+            adult.value = parseInt(adult.value) + 1;
+            minus_adult.disabled = false;
+            minus_adult.style.color = 'blue';  // increementing child will increement 1 adult count as each room should contain atleast 1 adult
+        }
+        room.value = parseInt(room.value) + 1;
+        children.value = parseInt(children.value) + 1;
+        if(room.value == 5) {
+            plus_room.disabled = true;
+            plus_room.style.color = 'black';
+        }
     }
-
-}
-
-// Function to decreement the count of the adult number
-function decreement_adult() {
-    document.getElementById("btn4").disabled = false;
-    adult = document.getElementById("number2").value;
-    room = document.getElementById("number1").value;
-    document.getElementById("btn5").disabled = false;
-    document.getElementById("btn6").disabled = false;
-    --adult;                                            // decreement the count of the adult 
-    if (adult<room) {                                   // if count of adult goes below the count of room 
-        document.getElementById("btn4").disabled = true;   // the decreement button of the adult is disabled as the condition states that each room should contain atleast one adult
-        document.getElementById("btn4").style.color = 'black'; // indicating the same
-    }
-    else {                                                  // if count of adult is greater than the room count
-        document.getElementById("number2").value = adult;   // decreement the adult count
-        document.getElementById("btn3").disabled = false;   // enable the increement button of the adult
-        document.getElementById("btn3").style.color = 'red';
-    }
-
-}
-
-let children = document.getElementById("number3");
-children.value = 0;       // assigning the value of the children to 0
-
-// Function to increement the count of the children 
-function increement_children() {
-    children = document.getElementById("number3").value;
-    let re = summ();                                // initialising the variable re and assigning the value returned from the function summ
-    if(re!=0) {                                     // if re is not equal to 0
-        document.getElementById("number3").value = ++children;  // increement the count of the children
-        document.getElementById("btn6").disabled = false;
-        document.getElementById("btn6").style.color = 'blue';
-    }
-    else {                                                  // if re == 0
-        document.getElementById("btn5").disabled = true;    // disabling the increement button of the children
-        document.getElementById("btn5").style.color = 'black';
+    else if(return_type_2==2) {
+        children.value = parseInt(children.value) + 1;
+        plus_children.disabled = true;
+        plus_children.style.color = 'black';
+        plus_adult.disabled = true;
+        plus_adult.style.color = 'black';
     }
 }
 
-// Function to decreement the count of the children 
-function decreement_children() {
-    children = document.getElementById("number3").value;
-    if(children>0) {                                    // if count of the children is greater than 0
-        document.getElementById("number3").value = --children;  // decreement the count 
-        document.getElementById("btn5").disabled = false;
-        document.getElementById("btn5").style.color = 'red';
+function decrement_room() {
+    plus_room.disabled = false;
+    plus_room.style.color = 'red';
+    plus_adult.disabled = false;
+    plus_adult.style.color = 'red';
+    plus_children.disabled = false;
+    plus_children.style.color = 'red';
+    room.value = parseInt(room.value) - 1;
+    adult_count = parseInt(adult.value);
+    children_count = parseInt(children.value);
+    let members = total_members(0,adult_count,children_count);
+    if(parseInt(members) > 4*room_count) {
+        let difference = parseInt(members) - 4*room_count;
+        if (children_count > difference) {
+            children.value = parseInt(children.value) - difference;
+        }
+        if (children_count == difference) {
+            children.value = parseInt(children.value) - difference;
+            minus_children.disabled = true;
+            minus_children.style.color = 'black';
+        }
+        else {
+            adult.value = parseInt(adult.value) - (difference - parseInt(children.value));
+            children.value = 0;
+            minus_children.disabled = true;
+            minus_children.style.color = 'black';
+        }
     }
-    else {                                              // if count goes below 0
-        document.getElementById("btn6").disabled = true;    // disable the decreement button of the children
-        document.getElementById("btn6").style.color = 'black';
+    if(room.value == 1) {
+        minus_room.disabled = true;
+        minus_room.style.color = 'black';
+    }
+
+}
+
+function decrement_adult() {
+    plus_adult.disabled = false;
+    plus_adult.style.color = 'red';
+    plus_children.disabled = false;
+    plus_children.style.color = 'red';
+    adult_count = parseInt(adult.value);
+    room_count = parseInt(room.value);
+    children_count = parseInt(children.value);
+    let return_type_3 = total_members(-1,adult_count-1,children_count);
+    if(return_type_3 == 1) {
+        if(adult_count > room_count) {
+            adult.value = parseInt(adult.value) - 1;
+        }
+        else {
+            members = total_members(0,adult_count,children_count);
+            --room_count;
+            room.value = parseInt(room.value) - 1;
+            plus_room.disabled = false;
+            plus_room.style.color = 'red';
+            if(room.value == 1) {
+                minus_room.disabled = true;
+                minus_room.style.color = 'black';
+            }
+            if(parseInt(members) > 4*room_count) {
+                let difference = parseInt(members) - 4*room_count;
+                if (children_count >= difference) {
+                    children.value = parseInt(children.value) - difference + 1;
+                    adult.value = parseInt(adult.value) - 1;
+                }
+                else {
+                    adult.value = parseInt(adult.value) - (difference - parseInt(children.value)) - 1;
+                    children.value = 0;
+                }
+            }
+            else {
+                adult.value = parseInt(adult.value) - 1;
+            }
+        }  
+    }
+    if(return_type_3 == 0) {
+        room.value = parseInt(room.value) - 1;
+        adult.value = parseInt(adult.value) - 1;
+        plus_room.disabled = false;
+        plus_room.style.color = 'red';
+        if(room.value == 1) {
+            minus_room.disabled = true;
+            minus_room.style.color = 'black';
+        }
+    }
+    if(adult_count-1 == 1) {
+        minus_adult.disabled = true;
+        minus_adult.style.color = 'black';
     }
 }
 
-// Function to find number of people in a room
-function summ() {
-    room = document.getElementById("number1").value;
-    adult = document.getElementById("number2").value;
-    children = document.getElementById("number3").value;
-    let res = parseInt(adult) + parseInt(children);     // add count of both adult and children 
-
-    if(res < (parseInt(room)*4)) {              // if sum is less than 4*room count (condition where each room can occupy upto 4 members)
-        return res;                             // return the sum
+function decrement_children() {
+    plus_children.disabled = false;
+    plus_children.style.color = 'red';
+    plus_adult.disabled = false;
+    plus_adult.style.color = 'red';
+    adult_count = parseInt(adult.value);
+    children_count = parseInt(children.value);
+    let return_type_4 = total_members(-1,adult_count,children_count-1);
+    if(return_type_4 == 1) {
+        children.value = parseInt(children.value) - 1;
     }
-    else return 0;                              // if sum is greater then return 0
-}
-
-// Function to decreement the count of adult and children while decreementing the room count 
-function people(rn,an,cn) {
-    rn = parseInt(rn);
-    an = parseInt(an);
-    cn = parseInt(cn);
-    if((an+cn)<=(4*rn)) {       // if sum of both adult and children is lesser than 4*room count
-        document.getElementById("number1").value = rn;     // display the value as it is
+    if(return_type_4 == 0) {
+        room.value = parseInt(room.value) - 1;
+        children.value = parseInt(children.value) - 1;
+        plus_room.disabled = false;
+        plus_room.style.color = 'red';
+        if(room.value == 1) {
+            minus_room.disabled = true;
+            minus_room.style.color = 'black';
+        }
     }
-    else if ((an+cn)>(4*rn) && rn != 0) {       // if sum is greater than the 4*room count
-        document.getElementById("number1").value = rn;
-        document.getElementById("number2").value = Math.floor(an/2);    // decreement the adult count by half
-        document.getElementById("number3").value = Math.floor(cn/2);    // decreement the children count by half to satisfy the condition
-    }
-    else if (rn == 0) {                                 // if room count goes below 0
-        document.getElementById("btn2").disabled = true;    // disable the room decreement button 
-        document.getElementById("btn2").style.color = 'black';
+    if(children_count-1 == 0) {
+        minus_children.disabled = true;
+        minus_children.style.color = 'black';
     }
 }
 
+function total_members(type,adult_number,children_number) {
+    room_count = parseInt(room.value);
+    let total = adult_number + children_number;
+    if(type == +1) {
+        if(total <= 4*room_count && total!=20) {
+            return 1;
+        }
+        else if(total == 20) {
+            return 2;
+        }
+        else if(total > 4*room_count) {
+            minus_room.disabled = false;
+            minus_room.style.color = 'blue';
+            return 0;
+        }
+    }
+    if(type == -1) { 
+        if (total % 4 == 0) {
+            return 0;
+        }
+        else if (total % 4 != 0) {
+            return 1;
+        }
+    }
+    if(type == 0) {
+        return total;
+    }
+}
